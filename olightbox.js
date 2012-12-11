b1b6f3cd69d2b0eeb7be104d1b029823c808e459
Mattias Lyckne <mattias.lyckne@gmail.com>
2012-12-12 00:57:50 +0100

    Refactor

--- olightbox.js
+++ olightbox.js
@@ -9,6 +9,7 @@
 		var that = this;
 		this.$element = $(element);
 		this.fetchUrl = this.$element.attr("href");
+		this.overlay = $("<div />");
 		this.lightbox = $("<div />").addClass("olightbox-wrapper");
 		this.contentWrapper = $("<div />").addClass("olightbox-content-wrapper");
 		this.content = $("<img />");
@@ -16,7 +17,6 @@
 						width: window.innerWidth || $(window).width(),
 						height: window.innerHeight || $(window).height()
 						};
-		console.log(this.dimension);
 		this.options = $.extend({}, $.fn.olightbox.defaults, options);
 
 	};
@@ -24,30 +24,54 @@
 	Olightbox.prototype = {
 		Constructor	: Olightbox,
 
-		show : function() {
+		prepare : function() {
 			var that = this;
-			var overlay = $("<div />")
-				.addClass(this.options.elements.overlay.className)
-				.hide()
-				.appendTo("body")
-				.fadeIn();
 
-			$("<div />")
-				.addClass(this.options.elements.loader.className)
-				.appendTo(overlay);
-			this.content.hide().attr("src", this.fetchUrl)
-				.load( function() {
-					console.log("done");
-					$(this).fadeIn();
-					that.lightbox.css({
-						"left": (that.dimension.width-that.lightbox.width())/2,
-						"top": (that.dimension.height-that.lightbox.height())/2 
+			this.showLoader();
+			
+			if(this.isImage(this.fetchUrl)) {
+				this.content.attr("src", this.fetchUrl)
+					.load( function() {
+						$(this).fadeIn();
+						that.lightbox.css({
+							left: (that.dimension.width-that.lightbox.width())/2,
+							top: (that.dimension.height-that.lightbox.height())/2
+						});
 					});
-				}).fadeIn().appendTo(this.contentWrapper);
+			}
+			this.content.appendTo(this.contentWrapper);
 
 			this.contentWrapper.appendTo(this.lightbox);
-			this.lightbox.hide().appendTo("body").fadeIn();
+			this.lightbox.hide().appendTo("body");
 
+		},
+
+		showLoader : function() {
+			this.overlay
+				.addClass(this.options.elements.overlay.className)
+				.hide()
+				.appendTo("body");
+
+			if(this.overlay.children("."+this.options.elements.loader.className).length === 0) {
+				$("<div />")
+					.addClass(this.options.elements.loader.className)
+					.appendTo(this.overlay);
+			}
+		},
+
+		show : function() {
+			var that = this;
+			
+			this.prepare();
+			this.overlay.show();
+			this.lightbox.show();			
+			
+			this.bindToClose();
+		},
+
+
+		bindToClose : function() {
+			var that = this;
 			$("."+this.options.elements.overlay.className).on("click", function() {
 				that.remove.apply(that);
 			});
@@ -58,12 +82,15 @@
 			});
 		},
 
-
 		remove	: function() {
 			console.log("remove");
 			console.log(this);
 			$("."+this.options.elements.overlay.className).remove();
 			this.lightbox.remove();
+		},
+		
+		isImage: function (str) {
+			return typeof str === "string" && str.match(/(^data:image\/.*,)|(\.(jp(e|g|eg)|gif|png|bmp|webp)((\?|#).*)?$)/i);
 		}
 
 	};
