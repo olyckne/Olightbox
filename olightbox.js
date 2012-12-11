b83780e4455e5396512039af078a20ed47a0b2a6
Mattias Lyckne <mattias.lyckne@gmail.com>
2012-12-12 00:08:47 +0100

    external less/css, bug fixes

--- olightbox.js
+++ olightbox.js
@@ -6,6 +6,7 @@
 		CLASS
 	 */
 	var Olightbox = function(element, options) {
+		var that = this;
 		this.$element = $(element);
 		this.fetchUrl = this.$element.attr("href");
 		this.lightbox = $("<div />").addClass("olightbox-wrapper");
@@ -15,10 +16,9 @@
 						width: window.innerWidth || $(window).width(),
 						height: window.innerHeight || $(window).height()
 						};
-
+		console.log(this.dimension);
 		this.options = $.extend({}, $.fn.olightbox.defaults, options);
 
-		$(".overlay").on("click", this.remove);
 	};
 
 	Olightbox.prototype = {
@@ -27,31 +27,43 @@
 		show : function() {
 			var that = this;
 			var overlay = $("<div />")
-				.addClass("olightbox-overlay")
+				.addClass(this.options.elements.overlay.className)
 				.hide()
 				.appendTo("body")
 				.fadeIn();
 
 			$("<div />")
-				.addClass("olightbox-loader")
-				.css({"position" : "relative", "top": "50%"})
+				.addClass(this.options.elements.loader.className)
 				.appendTo(overlay);
 			this.content.hide().attr("src", this.fetchUrl)
 				.load( function() {
+					console.log("done");
 					$(this).fadeIn();
-					that.contentWrapper.css({
-						"left": (that.dimension.width-that.contentWrapper.width())/2,
-						"top": (that.dimension.height-that.contentWrapper.height())/2 
+					that.lightbox.css({
+						"left": (that.dimension.width-that.lightbox.width())/2,
+						"top": (that.dimension.height-that.lightbox.height())/2 
 					});
-				}).appendTo(this.contentWrapper);
+				}).fadeIn().appendTo(this.contentWrapper);
 
 			this.contentWrapper.appendTo(this.lightbox);
 			this.lightbox.hide().appendTo("body").fadeIn();
+
+			$("."+this.options.elements.overlay.className).on("click", function() {
+				that.remove.apply(that);
+			});
+
+			$(document).on("keydown", function() {
+				var key = event.key || event.which;
+				if(key === 27) that.remove();
+			});
 		},
 
 
 		remove	: function() {
-
+			console.log("remove");
+			console.log(this);
+			$("."+this.options.elements.overlay.className).remove();
+			this.lightbox.remove();
 		}
 
 	};
@@ -66,18 +78,28 @@
 				data = $this.data("olightbox"),
 				options = $.extend({}, $.fn.olightbox.defaults, $this.data(), 
 									typeof option == "object" && option);
-			console.log(option);
-
 			if(!data) $this.data("olightbox", (data = new Olightbox(this, options)));
 			if( typeof option == "string") data[option]();
-
+			else if(options.show) data.show();
 
 		});
 	};
 
 	$.fn.olightbox.defaults = {
 		"transition"	: "fadeIn",
-		"trigger"		: "click"
+		"trigger"		: "click",
+		"show"			: true,
+		'elements'		: {
+			'overlay'	: {
+				'className' : "olightbox-overlay"
+			},
+			'contentWrapper' : {
+				'className'	: 'olightbox-content-wrapper'
+			},
+			'loader' : {
+				'className'	:	'olightbox-loader'
+			}
+		}
 	};
 
 	$.fn.olightbox.Constructor = Olightbox;
@@ -85,9 +107,13 @@
 		
 	 */
 
-	$(document).on('click', '.olightbox', function(event) {
-		var $this = $(this);
-		$this.olightbox("show");
+	$(document).on($.fn.olightbox.defaults.trigger+'.olightbox', '[data-toggle="olightbox"]', function(event) {
+		var $this = $(this),
+			target = $this.attr("data-target") || $this.attr("href"),
+			option = {target: target};
+
+
+		$this.olightbox(option);
 
 		event.preventDefault();
 	});
